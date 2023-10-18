f8d71824410c0bcbaffdad55c085bc08dc709b6a
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-10-18 15:58:53 +0000

    removed dist

--- dist/routes/root.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function root(req, res) {
-    res.end(`<h1>Welcome to this graphql express api</h1>`);
-}
-exports.default = root;
